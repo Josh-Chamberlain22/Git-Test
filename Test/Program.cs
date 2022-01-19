a3399cf12c8bf17513c6b4498af4cdf63a25452f
Joshua Chamberlain <jospcham@ut.utm.edu>
2022-01-18 20:09:42 -0600

    Created a random number generator.

--- Test/Program.cs
+++ Test/Program.cs
@@ -6,8 +6,27 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
+            Console.WriteLine("I am thinking of a number between 0 and 100");
+            Random rnd = new Random();
+            int value = rnd.Next(0, 100);
+            Console.Write("Guess Number: ");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+            int counter = 0;
+            while (value != userValue) { 
+                if (value < userValue)
+                {
+                    Console.WriteLine("The number is too high. Please guess again");
+                    userValue = Convert.ToInt32(Console.ReadLine());
+                    counter++;
+                }
+                else
+                {
+                    Console.WriteLine("The number is too low. Please guess again");
+                    userValue = Convert.ToInt32(Console.ReadLine());
+                    counter++;
+                }
+            }
+            Console.WriteLine("You guessed Correct");
         }
     }
 }
